a5e7ab13374c8d08ffc943de7739e76369c5cf0e
Shivam Sharma <meshivam81@gmail.com>
2026-03-11 14:02:31 +0530

    fix: api response and set auth hook

--- aptify-frontend/src/features/auth/hooks/useAuth.js
+++ aptify-frontend/src/features/auth/hooks/useAuth.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
+import { login, register, logout } from "../services/auth.api";
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
@@ -8,4 +9,27 @@ export const useAuth = () => {
   }
 
   const { user, setUser, loading, setLoading } = context;
+
+  const handleLogin = async ({ email, password }) => {
+    setLoading(true);
+    const data = await login({ email, password });
+    setUser(data.user);
+    setLoading(false);
+  };
+
+  const handleRegister = async ({ username, email, password }) => {
+    setLoading(true);
+    const data = await register({ username, email, password });
+    setUser(data.user);
+    setLoading(false);
+  };
+
+  const handleLogout = async () => {
+    setLoading(true);
+    const data = await logout();
+    setUser(null);
+    setLoading(false);
+  }
+
+  return {user, loading, handleLogin, handleRegister, handleLogout}
 };

69804d00e2921d5caaed1c954923d3b12eaeb64f
Shivam Sharma <meshivam81@gmail.com>
2026-03-13 22:00:02 +0530

    feat(api): generate interview report with gemini (#12)
    * feat(api): generate interview report with gemini
    
    * cammel case

---
+++ aptify-backend/src/controllers/interview.controller.js
@@ -0,0 +1,24 @@
+import { InterviewReport } from "../models/interviewReport.model.js";
+import { generateInterviewReport } from "../services/ai.service.js";
+import pdfParse from "pdf-parse";
+
+export const generateInterviewReports = async (req, res) => {
+  const resumeContent = await new pdfParse.PDFParse(Uint8Array.from(req.file.buffer)).getText();
+  const { selfDescription, jobDescription } = req.body;
+
+  const generateReport = await generateInterviewReport({
+    resume: resumeContent.text,
+    selfDescription,
+    jobDescription,
+  });
+
+  const report = await InterviewReport.create({
+    user: req.user.id,
+    resume: resumeContent.text,
+    selfDescription,
+    jobDescription,
+    ...generateReport,
+  });
+
+  res.status(201).json({ message: "Interview report generated successfully.", report });
+};
--- aptify-backend/src/routes/auth.routes.js
+++ aptify-backend/src/routes/auth.routes.js
@@ -11,8 +11,26 @@ const router = Router();
  */
 
 router.post("/user/register", register);
+
+/**
+ * @route POST /api/auth/user/login
+ * @description Login user
+ * @access Public
+ */
 router.post("/user/login", login);
+
+/**
+ * @route POST /api/auth/user/logout
+ * @description Logout user
+ * @access Public
+ */
 router.post("/user/logout", logout);
+
+/**
+ * @route GET /api/auth/user/me
+ * @description Return authenticated user
+ * @access Public
+ */
 router.get("/user/me", authUser, getMe);
 
 export default router;

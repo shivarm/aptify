2da410c361b1964287fb526e35bae0d571fb2b44
Shivam Sharma <meshivam81@gmail.com>
2026-03-13 14:30:27 +0530

    feat(ai): setup google gemni service with zod scheam

---
+++ aptify-backend/src/services/ai.service.js
@@ -0,0 +1,88 @@
+import { GoogleGenAI } from "@google/genai";
+import { ENV } from "../config/env.js";
+import { z } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
+
+const { GEMNI_API_KEY } = ENV;
+
+if (!GEMNI_API_KEY) {
+  throw new Error("google gemni api key is missing!");
+}
+
+const ai = new GoogleGenAI({
+  apiKey: GEMNI_API_KEY,
+});
+
+const interviewReportSchema = z.object({
+  matchScore: z
+    .number()
+    .describe(
+      "A score between 0 and 100 indicating how well the candidate's profile matches the job describe",
+    ),
+  technicalQuestions: z
+    .array(
+      z.object({
+        question: z.string().describe("The technical question can be asked in the interview"),
+        intention: z.string().describe("The intention of interviewer behind asking this question"),
+        answer: z
+          .string()
+          .describe(
+            "How to answer this question, what points to cover, what approach to take etc.",
+          ),
+      }),
+    )
+    .describe(
+      "Technical questions that can be asked in the interview along with their intention and how to answer them",
+    ),
+  behavioralQuestions: z
+    .array(
+      z.object({
+        question: z.string().describe("The technical question can be asked in the interview"),
+        intention: z.string().describe("The intention of interviewer behind asking this question"),
+        answer: z
+          .string()
+          .describe(
+            "How to answer this question, what points to cover, what approach to take etc.",
+          ),
+      }),
+    )
+    .describe(
+      "Behavioral questions that can be asked in the interview along with their intention and how to answer them",
+    ),
+  skillGaps: z
+    .array(
+      z.object({
+        skill: z.string().describe("The skill which the candidate is lacking"),
+        severity: z
+          .enum(["low", "medium", "high"])
+          .describe(
+            "The severity of this skill gap, i.e. how important is this skill for the job and how much it can impact the candidate's chances",
+          ),
+      }),
+    )
+    .describe("List of skill gaps in the candidate's profile along with their severity"),
+  preparationPlan: z
+    .array(
+      z.object({
+        day: z.number().describe("The day number in the preparation plan, starting from 1"),
+        focus: z
+          .string()
+          .describe(
+            "The main focus of this day in the preparation plan, e.g. data structures, system design, mock interviews etc.",
+          ),
+        tasks: z
+          .array(z.string())
+          .describe(
+            "List of tasks to be done on this day to follow the preparation plan, e.g. read a specific book or article, solve a set of problems, watch a video etc.",
+          ),
+      }),
+    )
+    .describe(
+      "A day-wise preparation plan for the candidate to follow in order to prepare for the interview effectively",
+    ),
+  title: z.string().describe("The title of the job for which the interview report is generated"),
+});
+
+ 
+
+ 

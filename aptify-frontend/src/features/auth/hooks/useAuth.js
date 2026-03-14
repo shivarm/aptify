104b3f9d3d587000a342ab8daf2e42e02a455216
Shivam Sharma <meshivam81@gmail.com>
2026-03-14 13:35:40 +0530

    feat(ui): create attractive home page design (#13)

--- aptify-frontend/src/features/auth/hooks/useAuth.js
+++ aptify-frontend/src/features/auth/hooks/useAuth.js
@@ -33,9 +33,13 @@ export const useAuth = () => {
 
   useEffect(() => {
     const getAndSetUser = async () => {
-      const data = await getMe();
-      setUser(data.user);
-      setLoading(false);
+      try {
+        const data = await getMe();
+        setUser(data.user);
+      } catch (err) {
+      } finally {
+        setLoading(false);
+      }
     };
 
     getAndSetUser();

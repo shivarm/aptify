7b343604572ea96bad3e351bfb1d29958e947b5f
Shivam Sharma <meshivam81@gmail.com>
2026-03-14 21:43:47 +0530

    fix: api base path issue

--- aptify-frontend/src/lib/axios.js
+++ aptify-frontend/src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
-
 export const apiClient = axios.create({
-  baseURL: `${BASE_URL}/api`,
+  baseURL: import.meta.env.MODE === "development" ? `${API_BASE_URL}/api` : "/api",
   withCredentials: true,
 });
